909fb2134755f808f211b60ecf1f42f338ed7ce1
Ali <ali.hussein.pre@gmail.com>
2021-03-11 18:08:24 +0100

    feat (UI:font)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,21 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import '../css/global.css';
+import { extendTheme } from '@chakra-ui/react';
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        fontFamily: 'Nunito',
+      },
+    },
+  },
+});
 const MyApp: React.FC<{ Component: React.FC; pageProps: any }> = ({
   Component,
   pageProps,
 }): JSX.Element => {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <div className="bg">
         <Component {...pageProps} />
       </div>
