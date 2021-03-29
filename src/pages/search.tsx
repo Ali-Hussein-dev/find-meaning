e085be5c8471fe97e67f7f1f6b7f59f8c6a405bb
Ali <ali.hussein.pre@gmail.com>
2021-03-29 20:44:51 +0200

    feat (UI,rebuild) remove particle-bg, ctx, abstract comps

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,48 +1,47 @@
-import { SearchBar, WikiResponse } from '@/components/index';
+import { UrbanContainer, SearchBar, WikContainer } from '@/components/index';
 import * as React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ErrorBoundary } from 'react-error-boundary';
-import dynamic from 'next/dynamic';
-const BgParticleDynamic = dynamic(
-  () => import('../../src/components/index').then((com) => com.BgParticles),
-  { ssr: false },
-);
-//--------------------------------------
-interface CtxFreshData {
-  freshData: any;
-  setFreshData: (prvState: any) => void;
-}
 
-const queryClient = new QueryClient();
-export const Ctx = React.createContext<CtxFreshData | null>(null);
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+      keepPreviousData: true,
+    },
+  },
+});
+
 const Fallback = () => <div role="alert">Something went wrong!</div>;
+
+const ResponsesContainer = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SearchBar />
+      <ErrorBoundary FallbackComponent={Fallback}>
+        <WikContainer />
+        <UrbanContainer />
+      </ErrorBoundary>
+    </QueryClientProvider>
+  );
+};
 //=======================
 const SearchPage: React.FC = () => {
   // hooks
-  const [freshData, setFreshData] = React.useState<any>(null);
   //--------------------------------------
   // functions
   //--------------------------------------
   return (
-    <QueryClientProvider client={queryClient}>
-      <Ctx.Provider value={{ freshData, setFreshData }}>
-        <title>Find-Meaning | search</title>
-        <main className="flex flex-col items-center min-h-screen px-1 bg-gradient-blueGray">
-          <div
-            data-testid="search-page"
-            className="z-10 w-full px-1 sm:w-11/12 md:w-9/12 lg:w-8/12"
-          >
-            <SearchBar />
-            <ErrorBoundary FallbackComponent={Fallback}>
-              <WikiResponse />
-            </ErrorBoundary>
-          </div>
-          <div className="fixed inset-0 z-0 h-screen">
-            <BgParticleDynamic velocity={0.1} count={85} />
-          </div>
-        </main>
-      </Ctx.Provider>
-    </QueryClientProvider>
+    <main className="flex flex-col items-center min-h-screen px-1 bg-gradient-blueGray">
+      <title>Find-Meaning | search</title>
+      <div
+        data-testid="search-page"
+        className="z-10 w-full px-1 sm:w-11/12 md:w-9/12 lg:w-8/12"
+      >
+        <ResponsesContainer />
+      </div>
+    </main>
   );
 };
 export default SearchPage;
