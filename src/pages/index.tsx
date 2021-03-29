84aa3ce52e887b62b3b6c878f6e4971ab0c2e06c
Ali <ali.hussein.pre@gmail.com>
2021-03-29 20:42:00 +0200

    refactor (Naming): update api name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { CC, Input } from '@/components/index';
+import { CondComp, Input } from '@/components/index';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import * as React from 'react';
@@ -43,8 +43,8 @@ const HomeInput = () => {
           onChange={handleChange}
           cn="flex-grow text-blueGray-800 focus-within:bg-blueGray-300 opacity-90 bg-blueGray-700"
         />
-        <CC
-          isTrue={isIdle}
+        <CondComp
+          baseCond={isIdle}
           fallback={
             <span className="grid w-12 h-12 place-items-center text-blueGray-300">
               <Spinner />
@@ -61,7 +61,7 @@ const HomeInput = () => {
               <BsSearch className="mx-auto text-blueGray-100" />
             </button>
           </label>
-        </CC>
+        </CondComp>
       </form>
     </div>
   );
--- test/pages/index.test.tsx
+++ test/pages/index.test.tsx
@@ -3,8 +3,10 @@ import { render } from '../testUtils';
 import { Home } from '../../src/pages/index';
 
 describe('Home page', () => {
-  it('matches snapshot', () => {
-    const { asFragment } = render(<Home />, {});
+  it('Home page', () => {
+    const { container, asFragment } = render(<Home />);
+    expect(container).toBeInTheDocument();
     expect(asFragment()).toMatchSnapshot();
   });
 });
+export {};
