be635d7d8f90b96ec175fbacfebbe56bbb3da01f
Ali <ali.hussein.pre@gmail.com>
2021-04-12 10:02:56 +0200

    refactor(react-query-prov): add react-query prov

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,19 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import '../css/global.css';
 import { extendTheme } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
   import('@/test/__mocks__/index');
 }
-
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+      keepPreviousData: true,
+    },
+  },
+});
 const theme = extendTheme({
   styles: {
     global: {
@@ -20,8 +29,12 @@ const MyApp: React.FC<{ Component: React.FC; pageProps: any }> = ({
 }): JSX.Element => {
   return (
     <ChakraProvider theme={theme}>
-      <link rel="icon" href="/favicon.ico" />
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <link rel="icon" href="/favicon.ico" />
+        <div className="z-10 min-h-screen">
+          <Component {...pageProps} />
+        </div>
+      </QueryClientProvider>
     </ChakraProvider>
   );
 };
