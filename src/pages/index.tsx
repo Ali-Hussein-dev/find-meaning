fa7c01dc34319da8ee467dfd11e25be209d1e3aa
Ali <ali.hussein.pre@gmail.com>
2021-04-12 10:00:29 +0200

    refactor(DOM,react-query-prov): remove duplication

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,6 @@ import { SearchBar } from '@/components/index';
 import Head from 'next/head';
 import * as React from 'react';
 import dynamic from 'next/dynamic';
-import { queryClient } from './search';
-import { QueryClientProvider } from 'react-query';
 import store from 'store2';
 import { initialSuggestions } from 'src/utils';
 
@@ -19,27 +17,27 @@ export const Home = (): JSX.Element => {
   }
   storedKey;
   return (
-    <div className="grid h-screen place-items-center text-blueGray-600">
+    <main className="relative grid w-full min-h-screen place-items-center text-blueGray-600">
       <Head>
         <title>Find Meaning</title>
       </Head>
-      <main className="relative grid w-full min-h-screen place-items-center">
-        <QueryClientProvider client={queryClient}>
-          <div className="z-10 w-11/12 sm:w-10/12 md:w-6/12">
-            <div className="flex items-center justify-center">
-              <h1 className="mb-2 text-2xl font-semibold text-center text-orange-700">
-                Find-Meaning
-              </h1>
-              <span className="ml-1 text-xs text-blueGray-400">Demo</span>
-            </div>
-            <SearchBar pushRouter={true} />
-          </div>
-        </QueryClientProvider>
-        <div className="fixed inset-0 z-0 h-screen bg-gradient-blueGray">
-          <BgParticleDynamic />
+
+      <div className="z-10 w-11/12 sm:w-10/12 md:w-6/12">
+        <div className="flex items-center justify-center">
+          <h1 className="mb-2 text-2xl font-semibold text-center text-orange-700">
+            Find-Meaning
+          </h1>
+          <span className="ml-1 text-xs text-blueGray-400">Demo</span>
         </div>
-      </main>
-    </div>
+        {/* <QueryClientProvider client={queryClient}> */}
+        <SearchBar pushRouter={true} />
+        {/* </QueryClientProvider> */}
+      </div>
+
+      <div className="fixed inset-0 z-0 h-screen bg-gradient-blueGray">
+        <BgParticleDynamic />
+      </div>
+    </main>
   );
 };
 
