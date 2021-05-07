2d1d26928d9ed9a0745029311acc0d812a58ad82
Ali <ali.hussein.pre@gmail.com>
2021-05-07 04:06:18 +0200

    feat(mixed): recieve conjugated forms data through props
    move form container
    
    tweak ui

--- src/components/WikiLexeme.tsx
+++ src/components/WikiLexeme.tsx
@@ -1,7 +1,12 @@
-import { DefExm } from '@/components/index';
 import { AiTwotoneAppstore, AiTwotoneMinusSquare } from 'react-icons/ai';
-
-import { CondComp } from '@/components/index';
+import {
+  DefExm,
+  CondComp,
+  ConjugatedForms,
+  FormsContainer,
+} from '@/components/index';
+import { ConjugatedFormsT } from './ConjugatedForms';
+import { Tooltip } from '@chakra-ui/react';
 // interface Sense {
 //   definition: string;
 //   usageExamples?: string | string[];
@@ -20,30 +25,15 @@ export interface Lexeme {
   forms: any[];
   synonymSets: any[];
 }
-const FormsContainer: React.FC<{ baseCond: boolean }> = ({
-  baseCond,
-  children,
-}) => (
-  <CondComp
-    baseCond={baseCond}
-    className="flex flex-wrap items-start font-semibold rounded gap-x-2 text-blueGray-600"
-  >
-    <span className="px-3 py-[0.10rem] font-semibold rounded-full text-emerald-700 bg-emerald-100">
-      forms
-    </span>
-    {children}
-  </CondComp>
-);
+
 const LexemeContainer: React.FC<{ partOfSpeech: string; children: any }> = ({
   children,
   partOfSpeech,
 }) => {
   return (
-    <div className="mb-5">
-      <h3 className="mb-2">
-        <span className="px-3 font-semibold rounded-full bg-lightBlue-100 text-lightBlue-700 ">
-          {partOfSpeech}
-        </span>
+    <div className="mb-3 ">
+      <h3 className="py-1 pl-3 mb-2 font-semibold bg-lightBlue-100 rounded-xl text-lightBlue-800 ">
+        {partOfSpeech}
       </h3>
       {children}
     </div>
@@ -53,7 +43,8 @@ const LexemeContainer: React.FC<{ partOfSpeech: string; children: any }> = ({
 //=======================
 export const WikiLexeme: React.FC<{
   lexeme: Lexeme;
-}> = ({ lexeme }) => {
+  conjugatedFormsData: ConjugatedFormsT;
+}> = ({ lexeme, conjugatedFormsData }) => {
   //--------------------------------------hooks
   const { partOfSpeech, senses } = lexeme;
   if (senses.length > 3) {
@@ -74,25 +65,22 @@ export const WikiLexeme: React.FC<{
           ))}
           <FormsContainer baseCond={lexeme?.forms?.length > 0}>
             <span className="flex items-center gap-x-1">
-              <AiTwotoneMinusSquare className="text-blueGray-400" />
-              {lexeme?.lemma}
+              <AiTwotoneMinusSquare className="text-blueGray-300" />
+
+              <Tooltip hasArrow label="singular" aria-label="a tooltip">
+                {lexeme?.lemma}
+              </Tooltip>
             </span>
             <CondComp
               baseCond={lexeme?.forms?.[0]?.form}
               className="flex items-center gap-x-2"
             >
-              <AiTwotoneAppstore className="text-blueGray-400" />
-              {lexeme?.forms?.[0]?.form}
+              <AiTwotoneAppstore className="text-blueGray-300" />
+              <Tooltip hasArrow label="plural" aria-label="a tooltip">
+                {lexeme?.forms?.[0]?.form}
+              </Tooltip>
             </CondComp>
           </FormsContainer>
-        </LexemeContainer>
-      );
-    case 'verb':
-      return (
-        <LexemeContainer partOfSpeech={partOfSpeech}>
-          {senses.map((o, i) => (
-            <DefExm key={i} def={o.definition} exm={o.usageExamples} />
-          ))}
         </LexemeContainer>
       );
     case 'adjective':
@@ -105,10 +93,24 @@ export const WikiLexeme: React.FC<{
           </>
           <FormsContainer baseCond={lexeme?.forms?.length > 0}>
             {lexeme.forms?.map((o, i) => (
-              <CondComp key={i} baseCond={!!o.form}>
+              <CondComp key={i} baseCond={!!o.form} className="flex gap-x-2">
                 {o.form}
+                {lexeme.forms.length - 1 > i ? (
+                  <span className="text-blueGray-400">|</span>
+                ) : null}
               </CondComp>
             ))}
+          </FormsContainer>
+        </LexemeContainer>
+      );
+    case 'verb':
+      return (
+        <LexemeContainer partOfSpeech={partOfSpeech}>
+          {senses.map((o, i) => (
+            <DefExm key={i} def={o.definition} exm={o.usageExamples} />
+          ))}
+          <FormsContainer baseCond={!!conjugatedFormsData}>
+            <ConjugatedForms forms={conjugatedFormsData} />
           </FormsContainer>
         </LexemeContainer>
       );
