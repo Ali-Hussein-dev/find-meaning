3ccbb08ef473184fe005d110f6aa82fc5e1c9513
Ali <ali.hussein.pre@gmail.com>
2021-05-22 14:44:07 +0200

    feat(input): add forward ref

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
-interface InputProps extends React.ComponentPropsWithoutRef<'input'> {
+interface InputProps extends React.ComponentPropsWithRef<'input'> {
   cn?: string;
 }
+export type inputRefType = React.ForwardedRef<HTMLInputElement>;
 
 //=======================
-export const Input: React.FC<InputProps> = ({
-  cn = 'bg-blueGray-800 text-blueGray-200 focus:opacity-90 focus:bg-blueGray-700',
-  ...props
-}) => {
+const InputWithRef = (props: InputProps, ref: inputRefType) => {
+  const {
+    cn = 'bg-blueGray-800 text-blueGray-200 focus:opacity-90 focus:bg-blueGray-700 pl-3',
+  } = props;
   return (
     <>
       <label htmlFor="search-input" />
@@ -18,9 +19,11 @@ export const Input: React.FC<InputProps> = ({
         name="search-input"
         aria-label="search-input"
         placeholder="start looking up words..."
-        className={`w-full h-12 py-1 pl-3 text-lg disabled:text-trueGray-400 disabled:cursor-not-allowed focus:outline-none ${cn}`}
+        className={`w-full h-12 py-1 text-lg disabled:text-trueGray-400 disabled:cursor-not-allowed focus:outline-none ${cn}`}
+        ref={ref}
         {...props}
       />
     </>
   );
 };
+export const Input = React.forwardRef(InputWithRef);
