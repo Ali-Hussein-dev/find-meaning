eb0d43fdd84f43ca5951159b5e9b87ef53765dd5
Ali <ali.hussein.pre@gmail.com>
2021-06-01 20:43:23 +0200

    feat(db): retrieve suggestions from db

--- db/crud.ts
+++ db/crud.ts
@@ -22,3 +22,35 @@ export const findUser = async (db: Db, _id: string) => {
 export const addUser = async (db: Db, user: UserT) => {
   return await db.collection('feedback_popup').insertOne(user);
 };
+//--------------------------------------read-suggestions
+export const findByStartWith = async (db: Db, query: string) => {
+  return await db
+    .collection('autocomplete')
+    .aggregate([
+      { $match: { _id: query.charAt(0) } },
+      {
+        $project: {
+          list: {
+            $slice: [
+              {
+                $filter: {
+                  input: '$list',
+                  as: 'item',
+                  cond: {
+                    $regexFind: {
+                      input: '$$item.h',
+                      regex: `^${query}`,
+                      options: 'm',
+                    },
+                  },
+                },
+              },
+              0,
+              10,
+            ],
+          },
+        },
+      },
+    ])
+    .toArray();
+};
