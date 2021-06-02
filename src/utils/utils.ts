53146fd0f3109ed61110eeea2da69d41252b59c3
Ali <ali.hussein.pre@gmail.com>
2021-06-02 09:59:18 +0200

    chore(utils): remove unused utils functions

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,4 @@
 /* eslint-disable prefer-const */
-import throttle from 'lodash/throttle';
-import store from 'store2';
 import Axios, { AxiosResponse } from 'axios';
 export type keyQueryT = 'urban' | 'lingua' | 'giphy';
 //--------------------------------------fetcher_post
@@ -46,64 +44,6 @@ export const fetcher_get = async (config: FetcherGetParams): Promise<any> => {
     .catch((error) => {
       console.error(error);
     });
-};
-//---------------------------------------compare
-const compare = (a, b) => {
-  a = a.w;
-  b = b.w;
-  return a < b ? -1 : a > b ? 1 : 0;
-};
-//---------------------------------------getSuggestion
-export const getSuggestions = (
-  key: string,
-  inputValue: string,
-): Suggestion[] => {
-  let list = store.get(key) || [];
-  inputValue = inputValue.trim().toLowerCase();
-  if (inputValue === '' && list.length > 7) {
-    list = list.slice(Math.max(list.length - 7, 1)).reverse();
-    return list;
-  }
-  list = list.filter((o) => o?.w.startsWith(inputValue)).sort(compare);
-  if (list.length > 7) {
-    list = list.slice(Math.max(list.length - 7, 1)).reverse();
-  }
-  return list.reverse();
-};
-//---------------------------------------throttledGetSuggestion
-export const throttledGetSuggestions = throttle(getSuggestions, 5);
-//---------------------------------------storeSuggestion
-interface Suggestion {
-  w: string;
-}
-export const storeSuggestion = (
-  key: string,
-  inputValue: string,
-): never | Suggestion[] => {
-  inputValue = inputValue.trim().toLowerCase();
-  // conditions to store query
-  // it doesn't return error or empty response -urban or wiktionary
-
-  // check if existed
-  let list = getSuggestions(key, inputValue).filter((o) => o?.w === inputValue);
-  if (list.length === 0) {
-    store.add(key, [{ w: inputValue }]);
-  }
-  // update list value
-  list = getSuggestions(key, inputValue).filter((o) => o?.w === inputValue);
-  // make sure the query is stored
-  if (list.length === 1) {
-    return list;
-    // throw error when query isn't stored
-  } else if (list.length === 0) {
-    throw Error(
-      `>> storeSuggestions: ${inputValue} could not been stored in localStorage`,
-    );
-  } else {
-    throw Error(
-      '>> storeSuggestions: the returned list length has neiter 0 nor 1 value',
-    );
-  }
 };
 //--------------------------------------types
 type partOfSpeechT = 'verb' | 'noun' | 'adjective' | 'adverb' | 'preposition';
