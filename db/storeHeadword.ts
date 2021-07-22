43f0242dfdf7554c1c8aa24da1af4ad38e250df5
Ali <ali.hussein.pre@gmail.com>
2021-07-22 12:23:38 +0200

    function to store headowords

---
+++ db/storeHeadword.ts
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import type { Db } from 'mongodb';
+//--------------------------------------
+const findHeadword = async (db: Db, query: string): Promise<boolean> => {
+  const foundQuery = await db
+    .collection('autocomplete')
+    .find({ _id: query.charAt(0), 'list.h': query })
+    .project({ 'list.$': 1 })
+    //  or { list: { $slice: 1 } },
+    .toArray();
+
+  return foundQuery.length > 0 ? true : false;
+};
+
+//--------------------------------------
+const pushHeadword = async (db: Db, query: string) => {
+  await db
+    .collection('autocomplete')
+    .update({ _id: query.charAt(0) }, { $push: { list: { h: query } } });
+};
+
+//--------------------------------------
+export const storeHeadword = async (
+  db: Db,
+  query: string,
+): Promise<void | true> => {
+  const isQueryInDB = await findHeadword(db, query);
+  if (isQueryInDB) return;
+  if (!isQueryInDB) {
+    pushHeadword(db, query);
+    return true;
+  }
+};
