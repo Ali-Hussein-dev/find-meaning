636779bbd44ceddc39e1f5e3896d747607a6faca
Ali <ali.hussein.pre@gmail.com>
2021-07-29 23:59:18 +0200

    feat(UI): minor UI tweak

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer: React.FC = () => {
   //======================================return
   return (
-    <div className="sticky bottom-0 z-[-1] center w-full h-12 text-lg bg-blueGray-100 gap-x-4 font-light">
+    <div className="w-full h-12 text-lg font-light center bg-blueGray-100 gap-x-4">
       &copy; {new Date().getFullYear()} <span>|</span> Find-Meaning
     </div>
   );
